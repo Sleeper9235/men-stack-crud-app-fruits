e709b09eb7b08f453701f5e1ab842af0099b7166
Sleeper9235 <kylejyoumans@gmail.com>
2024-06-21 11:08:44 -0500

    established database connection

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
+// connect to dotenv and loads the enviorment variable from .env file
+const dotenv = require('dotenv').config()
+// connects to express
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+// log connection status to terminal on start
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
 
 // GET /
 app.get("/", async (req, res) => {
