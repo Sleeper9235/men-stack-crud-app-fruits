2cc679d6e7fe09e9fbfb26b4ae9f53fef0f32bba
Sleeper9235 <kylejyoumans@gmail.com>
2024-06-21 12:00:34 -0500

    part one complete

--- server.js
+++ server.js
@@ -12,10 +12,31 @@ mongoose.connection.on("connected", () => {
     console.log(`Connected to MongoDB ${mongoose.connection.name}`)
 })
 
+const Fruit = require('./models/fruit.js')
+
+//middleware
+app.use(express.urlencoded({ extended: false }))
+
 // GET /
 app.get("/", async (req, res) => {
     res.render('index.ejs');
 })
+// GET /fruits/new
+app.get("/fruits/new", (req, res) => {
+    res.render('./fruits/new.ejs')
+})
+
+// Post /fruits
+app.post("/fruits", async (req, res) => {
+    if (req.body.isReadyToEat === "on") {
+        req.body.isReadyToEat = true;
+    } else {
+        req.body.isReadyToEat = false;
+    }
+    await Fruit.create(req.body)
+    res.redirect('/fruits/new')
+})
+
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
